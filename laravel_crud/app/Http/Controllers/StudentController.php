ceec4ed705ca7eb6e4803b9af098b0192fff97a4
Bijan <bijan2059@gmail.com>
2024-01-27 18:47:24 +0545

    first commit

---
+++ laravel_crud/app/Http/Controllers/StudentController.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class StudentController extends Controller
+{
+    public function index(){
+        $students = Student::all();
+        if ($students->count()>0) {
+            return response()->json([
+                'status' => 200,
+                'students' => $students
+            ],200);
+        }else {
+            return response()->json([
+                'status' => 404,
+                'message' => 'no records found'
+            ],404);
+        }
+    }
+
+    public function store(Request $request){
+        $validator= Validator::make($request->all(),[
+            'name'  => 'required|max:100',
+            'age'   => 'required|numeric|between:1,150', 
+            'email' => 'required|email|max:100',
+            'gender'        => 'required|in:male,female,other', 
+            'date_of_birth' => 'required|date', 
+            // 'image'         => 'required|image|mimes:jpeg,png,jpg,gif|max:2048' 
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'message' => $validator->messages()
+            ],422);
+        }else {
+            $student = Student::create([
+                'name'  => $request->name,
+                'age'   => $request->age,
+                'email' => $request->email,
+                'gender'        => $request->gender,
+                'date_of_birth' => $request->date_of_birth,
+                // 'image'         => $request->file('image')->getClientOriginalName()
+            ]);
+            // $image = $request->file('image')->store('uploads');
+        }
+        if ($student) {
+            return response()->json([
+                'status' => 200,
+                'message' => 'Student added successfully'
+            ],200);
+        }else {
+            return response()->json([
+                'status' => 500,
+                'message' => 'Something went wrong'
+            ],500);
+        }
+    }
+
+    public function show(int $id){
+        $student = Student::find($id);
+        if ($student) {
+            return response()->json([
+                'status' => 200,
+                'student' => $student
+            ],200);
+        }else{
+            return response()->json([
+                'status' => 404,
+                'message' => 'no such student found'
+            ],404);
+        }
+    }
+
+    public function edit(int $id){
+        $student = Student::find($id);
+        if ($student) {
+            return response()->json([
+                'status' => 200,
+                'student' => $student
+            ],200);
+        }else{
+            return response()->json([
+                'status' => 404,
+                'message' => 'no such student found'
+            ],404);
+        }
+    }
+
+    public function update(Request $request,int $id){
+        $validator= Validator::make($request->all(),[
+            'name'  => 'required|max:100',
+            'age'   => 'required|numeric|between:1,150', 
+            'email' => 'required|email|max:100',
+            'phone' => 'required|numeric|digits_between:10,15'
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'message' => $validator->messages()
+            ],422);
+        }else {
+            $student = Student::find($id);
+            if ($student) {
+                $student->update([
+                    'name'  => $request->name,
+                    'age'   => $request->age,
+                    'email' => $request->email,
+                    'phone' => $request->phone,
+                ]);
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Student updated successfully'
+                ],200);
+            }else {
+                return response()->json([
+                    'status' => 500,
+                    'message' => 'Something went wrong'
+                ],500);
+            }
+        }
+    }
+
+    public function delete(int $id){
+        $student = Student::find($id);
+        if ($student) {
+            $student->delete();
+            return response()->json([
+                'status' => 200,
+                'message' => 'student deleted successfully'
+            ],200);
+        }else{
+            return response()->json([
+                'status' => 404,
+                'message' => 'no such student found'
+            ],404);
+        }
+    }
+
+}
---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import axios from 'axios';
+function App() {
+  const [formData, setFormData] = useState({
+    name: '',
+    age: '',
+    email: '',
+    gender: '',
+    date_of_birth: '',
+    // image: {}
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    })
+  }
+
+  //for image handling
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setFormData({
+      ...formData,
+      image: file
+    });
+  }
+
+  const addData = async (formData) => {
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/api/students', formData);
+      if (response.status === 200) {
+        alert(response.data.message);
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //adding the data on submit
+    addData(formData);
+  }
+
+  return (
+    <>
+      <form action="" method="post" encType="multipart/form-data" onSubmit={handleSubmit}>
+        Name<br />
+        <input type="name" id="name" placeholder="Enter name"
+          value={formData.name} onChange={handleChange}
+          required /> <br />
+        Age <br />
+        <input type="number" id="age" placeholder="Enter age"
+          value={formData.age} onChange={handleChange}
+          required /> <br />
+        Email <br />
+        <input type="email" className="form-control" id="email" placeholder="Enter email"
+          value={formData.email} onChange={handleChange}
+          required /> <br />
+        Gender <br />
+        <input type="radio" id="gender" value='male'
+          checked={formData.gender === 'male'} onChange={handleChange} />Male
+        <input type="radio" id="gender" value='female'
+          checked={formData.gender === 'female'} onChange={handleChange} />Female <br />
+        Date of Birth <br />
+        <input type="date" id="date_of_birth"
+          value={formData.date_of_birth} onChange={handleChange} /> <br />
+        {/* Image <br /> */}
+        {/* <input type="file" id="image" onChange={handleImageChange} /> <br /> */}
+        <button type="submit">Submit</button>
+      </form>
+
+    </>
+
+  )
+}
+
+export default App
